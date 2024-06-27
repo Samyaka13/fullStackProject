0f3eba1790f844e055dcc04e087f9fa03af061e1
Samyaka13 <samyakajmera022@gmail.com>
2024-06-27 18:59:51 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+  app.on("error",(error)=>{
+            console.log("ERR: ",error);
+            throw error
+          })
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is running at post: ${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.log("MONGO DB connection failed!!! ",err);
+})
 
 
 
---
+++ src/utils/asyncHandles.js
@@ -0,0 +1,34 @@
+const asyncHandler = (requestHandler) => {
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err));
+    }
+};
+export { asyncHandler };
+
+
+
+
+
+
+
+
+
+
+
+////////////////////or we can do this 
+// const asyncHandler =  (fn)=> async (req,res,next) =>{
+//     try{
+//         await fn(req,res,next)
+//     }catch(error){
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// } //What is this is explained below
+
+
+
+
+
+// const explain = (func) => {()=>{}}  Explaination
